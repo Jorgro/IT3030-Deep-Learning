9328c361bd0c5c9b564157712f9b3c9b053571cb
Jørgen Rosager <joergen.rosager@gmail.com>
2022-02-06 15:52:53 +0100

    remove todos

--- Project1/main.py
+++ Project1/main.py
@@ -4,9 +4,6 @@ from network import NeuralNetwork
 # TODO:
 # - PREP FOR DEMO
 # - Read through delivery and make sure everything is there!
-# - Double check Cross entropy loss function
-# - Verbose flag
-# - Validate and test to plot
 if __name__ == "__main__":
     print("Loading config.")
     with open("Project1/config.json") as f:
