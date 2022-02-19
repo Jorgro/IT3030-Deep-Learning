dbaa4e0fc3ac9899ea8614314c151d7fa48c9584
Jørgen Rosager <joergen.rosager@gmail.com>
2022-02-19 15:55:06 +0100

    1d autoencoder decoder working

---
+++ Project2/encoder.py
@@ -0,0 +1,43 @@
+from torch import nn
+
+
+class Encoder(nn.Module):
+    def __init__(self, encoded_space_dim, fc2_input_dim):
+        super().__init__()
+
+        ### Convolutional section
+        self.encoder_cnn = nn.Sequential(
+            # First convolutional layer
+            nn.Conv2d(1, 8, 3, stride=2, padding=1),
+            # nn.BatchNorm2d(8),
+            nn.ReLU(True),
+            # Second convolutional layer
+            nn.Conv2d(8, 16, 3, stride=2, padding=1),
+            nn.BatchNorm2d(16),
+            nn.ReLU(True),
+            # Third convolutional layer
+            nn.Conv2d(16, 32, 3, stride=2, padding=0),
+            # nn.BatchNorm2d(32),
+            nn.ReLU(True),
+        )
+
+        ### Flatten layer
+        self.flatten = nn.Flatten(start_dim=1)
+
+        ### Linear section
+        self.encoder_lin = nn.Sequential(
+            # First linear layer
+            nn.Linear(3 * 3 * 32, 128),
+            nn.ReLU(True),
+            # Second linear layer
+            nn.Linear(128, encoded_space_dim),
+        )
+
+    def forward(self, x):
+        # Apply convolutions
+        x = self.encoder_cnn(x)
+        # Flatten
+        x = self.flatten(x)
+        # # Apply linear layers
+        x = self.encoder_lin(x)
+        return x
