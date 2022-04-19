e0132af6df28eee94fdf8ecfe9bc78a85ebec61c
Jørgen Rosager <joergen.rosager@gmail.com>
2022-04-19 09:00:03 +0200

    nice work on data preprocessing

---
+++ Project3/preprocessing.py
@@ -0,0 +1,52 @@
+from typing import List
+import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
+
+
+def filter_column_based_on_quantile(df, q, columns):
+    for col in columns:
+        q_low = df[col].quantile(q)
+        q_hi = df[col].quantile(1 - q)
+        df = df[(df[col] < q_hi) & (df[col] > q_low)]
+    return df
+
+
+def add_date_time_features(df):
+    # Convert start time to a Datatime-object which is much easier to work with
+    df["start_time"] = pd.to_datetime(df["start_time"], format="%Y-%m-%d %H:%M:%S")
+    df["time_of_day"] = df["start_time"].dt.hour
+    df["time_of_week"] = df["start_time"].dt.dayofweek
+    df["time_of_year"] = df["start_time"].dt.month - 1
+
+    # This is just to create atleast one of each possible unique value so get_dummies will work even
+    # though the dataframe might not have all possible unique values
+    df_date_features = pd.DataFrame()
+    df_date_features["time_of_day"] = pd.Series(list(range(0, 24)))
+    df_date_features["time_of_week"] = pd.Series(list(range(0, 7)))
+    df_date_features["time_of_year"] = pd.Series(list(range(0, 12)))
+    df_date_features.fillna(0)
+
+    # One hot encode data-time columns
+    temp = pd.get_dummies(
+        pd.concat([df, df_date_features], keys=[0, 1]),
+        columns=["time_of_day", "time_of_week", "time_of_year"],
+    )
+
+    # Selecting data from multi index
+    df = temp.xs(0)
+    return df
+
+
+def normalize_columns(df, columns: List[str]):
+    scaler = MinMaxScaler()
+    df[columns] = scaler.fit_transform(df[columns])
+    return df
+
+
+def normalize_based_on_other_df(
+    df_to_transform, df_to_fit, columns
+):
+    scaler = MinMaxScaler()
+    scaler.fit(df_to_fit[columns])
+    df_to_transform[columns] = scaler.transform(df_to_transform[columns])
+    return df_to_transform
---
+++ Project3/settings.py
@@ -0,0 +1,12 @@
+COLUMNS_TO_CLAMP = ["y"]
+COLUMNS_TO_NORMALIZE = [
+    "hydro",
+    "micro",
+    "thermal",
+    "wind",
+    "total",
+    "y",
+    "sys_reg",
+    "flow",
+]
+COLUMNS_TO_DROP = ["start_time", "river"]
